03350f83b5057945436cc00b1dcc39bd6ec923c0
ika <irene.atamian@gmail.com>
2016-07-20 12:11:18 +0000

    adding exercises from Flow Control chapter and revised Variables name

---
+++ Variables/name.rb
@@ -0,0 +1,24 @@
+#Exercise 1
+
+puts "Please enter your name:"
+name = gets.chomp
+puts "Hi there " + name
+
+#Exercise 3
+ 10.times do 
+    puts.name
+ end
+
+#Exercise 4
+puts "Please enter your first name:"
+first_name = gets.chomp
+puts "Please enter your last name:"
+last_name = gets.chomp
+puts "Your full name is:" + first_name + " " + last_name
+
+#Exercise 5
+# x prints out 0
+# x prints out error "undefined local varialbe or method", because x is initialized inside the do-end block and therefore not available outside of this block
+
+#Exercise 6
+# The error is telling you that a variable or method named shoes is trying to be referenced inside the program but has not been defined in the program or is not within the scope that it is being called
