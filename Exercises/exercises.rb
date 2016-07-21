78f869dd2e767c903eab33d02edb79fe0c9c3889
ika <irene.atamian@gmail.com>
2016-07-21 19:58:30 +0000

    adding excerises for remainging chapters

--- Exercises/exercises.rb
+++ Exercises/exercises.rb
@@ -42,3 +42,56 @@ h = {subjects: [math , science, art]}
 arr = [{subject: "math"}, {subject: "science"}]
 
 #Exercise 11
+#I found this and this: http://api.rubyonrails.org/
+#I liked this api as it reminds my of the core Ruby Api we have been using, 
+#with key clickable terms on the lefthand side and detailed descriptions on the righthand side.
+
+#Exercise 12
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
+            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+contacts["Joe Smith"][:email] = contact_data[0][0]
+contacts["Sally Johnson"][:email] = contact_data[1][0]
+contacts["Joe Smith"][:address] = contact_data[0][1]
+contacts["Sally Johnson"][:address] = contact_data[1][1]
+contacts["Joe Smith"][:phone] = contact_data[0][2]
+contacts["Sally Johnson"][:phone] = contact_data[1][2]
+
+#Exercise 13
+puts contacts["Joe Smith"][:email] 
+# or to identify Joe write: puts "Joe's email is: #{contacts["Joe Smith"][:email]}"
+puts contacts["Sally Johnson"][:phone] 
+# or to identify Sally write: puts "Sally's phone is: #{contacts["Sally Johnson"][:phone]}"
+
+#Exercise 14
+contact_data = ["joe@email.com", "123 Main st.", "555-123-4567"]
+contacts = {"Joe Smith" => {}}
+information = [:email, :address, :phone]
+contacts.each do |k, v|
+  information.each do |info|
+    v[info] =contact_data.shift
+  end  
+end   
+
+#Exercise 15
+arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
+arr.delete_if { |word| word.start_with?("s") }     
+
+arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
+arr.delete_if { |word| word.start_with?("s", "w") }
+
+
+#Exercise 16 
+a = ['white snow', 'winter wonderland', 'melting ice',
+     'slippery sidewalk', 'salted roads', 'white trees']
+a.map { |x| x.split}
+a = a.flatten
+p a
+
+#Exercise 17
+#These hashes are the same!
+
+
+
+
